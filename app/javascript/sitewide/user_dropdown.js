b89df8a34a70b2a42b1d213cbb8d5444f597e6ad
Ansel Rognlie <ansel.rognlie@gmail.com>
2020-05-20 10:39:29 -0700

    Switches user dropdown menu to use bootstrap

--- app/javascript/sitewide/user_dropdown.js
+++ app/javascript/sitewide/user_dropdown.js
@@ -1,7 +1,12 @@
-$('document').ready(() => {
-  const dd = $('span.nav_header__user--dropdown');
-  const menu = dd.parent().children('div.nav_header__user--menu');
-  dd.click(() => {
+$(document).on('turbolinks:load', () => {
+  console.log('ready');
+  const dd = $('span.nav_header__user--dropdown').parent();
+  const menu = dd.children('div.nav_header__user--menu');
+  dd.mouseover(() => {
     menu.css('display', 'block');
+  });
+
+  menu.mouseleave(() => {
+    menu.css('display', 'none');
   });
 });
