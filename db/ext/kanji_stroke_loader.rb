8b47300c3259b92cd01de5f046ee2b14ac36c933
Ansel Rognlie <ansel.rognlie@gmail.com>
2020-06-04 10:57:44 -0700

    Changes source of kanjivg dowload location
    Previously, we queried GitHub for information about the
    latest kanjivg release, but in order to reduce the
    complexity, we now just get the url from our data source
    configuration, as this will allow us to point to a new
    source of kanjivg data just by updating a url that
    we control.

--- db/ext/kanji_stroke_loader.rb
+++ db/ext/kanji_stroke_loader.rb
@@ -3,49 +3,48 @@
 require_relative 'step_writer'
 require_relative 'path_utils'
 require_relative 'zip_archive'
-require_relative 'github_import'
+require_relative 'step_writer'
+require_relative 'data_source_config'
 
 module KTL
-  TEMP_DIR = 'tmp_kvg'
+  TEMP_DIR = 'tmp_import'
   TEMP_ARCHIVE = 'main.zip'
 
   module_function
 
-  # rubocop: disable Metrics/MethodLength
+  # rubocop: disable Metrics/MethodLength, Metrics/AbcSize
   def main
     PathUtils.clean_path(TEMP_DIR, recreate: true)
 
-    api = GithubApi.new
+    data_source = DataSourceConfig.new
+    kanjivg_url = nil
+    StepWriter.log('Loading import locations...') do
+      config = data_source.config
+      kanjivg_url = config&.dig(:kakitori, :sources, :kanjivg, :url)
+    end
+
+    return unless kanjivg_url
+
     response = nil
-    StepWriter.log('Downloading kanjivg archive...', true) do
-      response = api.latest_main_release
+    StepWriter.log('Downloading kanjivg archive...', long: true) do
+      response = HTTParty.get(kanjivg_url)
     end
 
     return unless response
 
     out_archive = File.join(TEMP_DIR, TEMP_ARCHIVE)
-    File.open(out_archive, 'wb') do |file|
-      file.write response.body
+    StepWriter.log('Writing to disk...', long: true) do
+      File.open(out_archive, 'wb') do |file|
+        file.write response.body
+      end
     end
 
-    archive = ZipArchive.new(out_archive, to: TEMP_DIR)
+    archive = ZipArchive.new(out_archive, to: TEMP_DIR, every: 100)
     archive.extract
   ensure
     PathUtils.clean_path(TEMP_DIR)
   end
-  # rubocop: enable Metrics/MethodLength
+  # rubocop: enable Metrics/MethodLength, Metrics/AbcSize
 end
 
 KTL.main
-
-# StepWriter.log('Counting up...', true) do
-#   100_000_000.times {}
-# end
-
-# writer = StatusWriter.new
-# writer.start(
-#   count: 100_000,
-#   template: 'Processing %<step>d (%<num>d of %<total>d, %<percent>d%%)'
-# )
-# 100_000.times { |i| writer.next_step(i + 1) }
-# writer.done
--- db/ext/zip_archive.rb
+++ db/ext/zip_archive.rb
@@ -10,10 +10,11 @@ module KTL
   class ZipArchive
     STATUS_MSG = 'Extracting %<step>s (%<num>d of %<total>d, %<percent>d%%)'
 
-    def initialize(filename, writer = nil, to: nil)
+    def initialize(filename, writer = nil, to: nil, every: 1)
       @filename = filename
       @to = to
       @writer = writer || StatusWriter.new
+      @every = every
     end
 
     def entry_name(entry)
@@ -27,19 +28,18 @@ module KTL
     end
 
     def extract
-      StepWriter.log('Reading archive...', true)
+      StepWriter.log('Reading archive...', long: true)
       Zip::File.open(@filename) do |zip_file|
         StepWriter.done
-        @writer.start(count: zip_file.size, template: STATUS_MSG)
+        @writer.start(count: zip_file.size, template: STATUS_MSG, every: @every) do
+          # Handle entries one by one
+          zip_file.each do |entry|
+            @writer.next_step entry.name
 
-        # Handle entries one by one
-        zip_file.each do |entry|
-          @writer.next_step entry.name
-
-          # Extract to file or directory based on name in the archive
-          safe_extract(entry)
+            # Extract to file or directory based on name in the archive
+            safe_extract(entry)
+          end
         end
-        @writer.done
       end
     end
   end
