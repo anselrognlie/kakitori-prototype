5079b015c8e010eaf066ea205b76a123a4b3c4d7
Ansel Rognlie <ansel.rognlie@gmail.com>
2020-06-05 15:37:32 -0700

    Removes separate file for joyo and jlpt enums

--- app/models/jlpt_level.rb
+++ app/models/jlpt_level.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
+module JlptLevelEnum
+  UNDEF = 0
+  TEN = 1
+  NINE = 2
+  EIGHT = 3
+  SEVEN = 4
+  SIX = 5
+  FIVE = 6
+  FOUR = 7
+  THREE = 8
+  TWO_AND_A_HALF = 9
+  TWO = 10
+  ONE_AND_A_HALF = 11
+  ONE = 12
+end
+
 class JlptLevel < NamedEnum
   class << self
     # rubocop: disable Metrics/MethodLength
